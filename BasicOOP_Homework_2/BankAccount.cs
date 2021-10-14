785438290cc9878b993487eb7dc402a3c4cf0817
OlegErmoshchikov <ermoshchikov@gmail.com>
2021-10-14 16:10:55 +0300

    1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип). Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.

---
+++ BasicOOP_Homework_2/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BasicOOP_Homework_2
+{
+    enum AccountType        // виды банковский счетов
+    {
+        Current,       //текущмй счет
+        Savings,       //сберегательный счет
+        Checking       //расчетный счет
+
+    }
+    class BankAccount
+    {
+        private int numberAccount;
+        private decimal balance;
+        private AccountType type;
+
+        public BankAccount(int NumberAccount, decimal Balance, AccountType Type)
+        {
+            this.numberAccount = NumberAccount;
+            this.balance = Balance;
+            this.type = Type;
+        }
+
+        public int NumberAccount
+        {
+            get { return this.numberAccount = NumberAccount; }
+            set { this.numberAccount = value; }
+        }
+        public decimal Balance
+        {
+            get { return this.balance = Balance; }
+            set { this.balance = value; }
+        }
+        public AccountType Type
+        {
+            get { return this.type = Type; }
+            set { this.type = value; }
+        }
+
+        public void GetAccountInfo()
+        {
+            Console.WriteLine($"Ваш номер счета:{numberAccount} , баланс:{balance}рублей" +
+                              $" и тип банковского счета: {type}");
+        }
+    }
+}
---
+++ BasicOOP_Homework_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicOOP_Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount(40817810, 354000, AccountType.Current);
+            myAccount.GetAccountInfo();
+        }
+    }
+}
