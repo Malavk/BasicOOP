86a1e23f21ec63661d76b20bb2e3bc8996399d01
OlegErmoshchikov <ermoshchikov@gmail.com>
2021-11-19 23:45:38 +0300

    Немного не разобрался в теме, доделаю и обновлю репозиторий, сдаю пока то что есть

---
+++ BasicOOP_Homework_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicOOP_DZ_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumbers rationalNumbers1 = new RationalNumbers(1,4);
+            RationalNumbers rationalNumbers2 = new RationalNumbers(7,15);
+
+            Console.WriteLine(rationalNumbers1+rationalNumbers2);
+            Console.WriteLine(rationalNumbers1 % rationalNumbers2);
+        }
+    }
+}
