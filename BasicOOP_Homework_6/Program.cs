66c0c0d716d09e1e6f917f6b038c02e70e1158b5
OlegErmoshchikov <ermoshchikov@gmail.com>
2021-12-03 18:40:01 +0300

    Авансом кидаю пул, чтобы в ближайшее время заполнить его выполненным домашним заданием и не писать много раз в техподдержку. Заранее прошу понять и простить за задержку

---
+++ BasicOOP_Homework_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicOOP_DZ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Moto v = new Moto();
+            Console.WriteLine("Speed: {0}mph", v.Speed);    
+            v.Accelerate(25);
+            Console.WriteLine("Speed: {0}mph", v.Speed);    
+            v.Decelerate(15);
+            Console.WriteLine("Speed: {0}mph", v.Speed);
+        }
+    }
+}
